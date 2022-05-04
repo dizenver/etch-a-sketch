75e0b9f791ce0739a91bdfd1505e459a02866ecf
Denver Martinez <denver.martinez@gmail.com>
2022-05-04 00:39:22 -0500

    Add Slider to change grid size.

--- sketch.js
+++ sketch.js
@@ -1,14 +1,44 @@
 const etchasketch = document.createElement('div');
 etchasketch.classList.add('sketchContainer');
+etchasketch.setAttribute('id', 'etchasketch');
 document.body.appendChild(etchasketch);
 
-/* grid size */
-let length = 50;
+/* grid size slider */
+const gridSlider = document.createElement('input');
+gridSlider.classList.add('gridSlider');
+gridSlider.setAttribute('type', 'range');
+gridSlider.setAttribute('min', '1');
+gridSlider.setAttribute('max', '100');
+gridSlider.setAttribute('value', '20');
+gridSlider.setAttribute('id', 'slider');
+gridSlider.setAttribute('onchange', 'sliderValue.value=value');
+document.body.appendChild(gridSlider);
+
+const gridSelection = document.createElement('output');
+gridSelection.setAttribute('id','sliderValue');
+gridSelection.textContent = 16;
+document.body.appendChild(gridSelection);
+
+
+let length = 16;
+
+
+/*Watch Slider for Changes w/ Event Listener */
+const watchSlider = document.querySelector('.gridSlider');
+watchSlider.addEventListener('change', (event) => {
+    let length = document.getElementById("sliderValue").value;
+    let oldBlocks = document.getElementById("etchasketch");
+        while (oldBlocks.firstChild) {
+        oldBlocks.removeChild(oldBlocks.firstChild);
+        }
+    makeGrid(length);  
+  });
 
 /* Create Row with of length */
 function firstRow(length) {
     const rowLength = document.createElement('div');
     rowLength.classList.add('gridRow');
+    rowLength.setAttribute('id','rowLength');
     etchasketch.appendChild(rowLength);
 
     for (let i = 1; i <= length; i++) {
