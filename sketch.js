e63527e7bdde7b8926a87e653fe44b985577daf0
Denver Martinez <denver.martinez@gmail.com>
2022-05-03 23:36:27 -0500

    Add random color on hover and comments for organization.

--- sketch.js
+++ sketch.js
@@ -2,9 +2,10 @@ const etchasketch = document.createElement('div');
 etchasketch.classList.add('sketchContainer');
 document.body.appendChild(etchasketch);
 
-
+/* grid size */
 let length = 50;
 
+/* Create Row with of length */
 function firstRow(length) {
     const rowLength = document.createElement('div');
     rowLength.classList.add('gridRow');
@@ -18,18 +19,31 @@ function firstRow(length) {
     }
 
 }
-
-function repeatRows(length) {
+/* Repeat row 'length' times to create grid*/
+function makeGrid(length) {
 
     for (let i = 1; i <= length; i++) {
          firstRow(length);
     }
 }
 
-repeatRows(length);  
+/*Create Grid */
+makeGrid(length);  
 
+
+/* Generate Random Color */
+function blockBg () {
+    const x=Math.round(0xffffff * Math.random()).toString(16);
+    const y=(6-x.length);
+    const z= "000000";
+    const z1 = z.substring(0,y);
+    const color = "#" + z1 + x;
+    return color; 
+}
+
+/* Call Random Color */
 function hoverColor($event) {
     let block = $event.target;
-    block.style.backgroundColor = 'blue';
+    block.style.backgroundColor = `${blockBg()}`;
 }
 
